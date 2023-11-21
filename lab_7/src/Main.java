710f562491416c878df2dd82f82d60c280e9c25a
afafos <afanasyev239@gmail.com>
2023-11-21 20:57:18 +0300

    Update lab_7

--- lab_7/src/Main.java
+++ lab_7/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        MyClass obj1 = new MyClass(10);
-        MyClass obj2 = new MyClass(20);
+        MyClass<Integer> obj1 = new MyClass<>(10);
+        MyClass<Integer> obj2 = new MyClass<>(20);
 
         System.out.println("До обмена:");
         System.out.println("obj1: " + obj1.getValue());
@@ -15,25 +15,25 @@ public class Main {
         System.out.println("obj2: " + obj2.getValue());
     }
 
-    public static void swapObjects(MyClass a, MyClass b) {
-        int temp = a.getValue();
+    public static <T> void swapObjects(MyClass<T> a, MyClass<T> b) {
+        T temp = a.getValue();
         a.setValue(b.getValue());
         b.setValue(temp);
     }
 }
 
-class MyClass {
-    private int value;
+class MyClass<T> {
+    private T value;
 
-    public MyClass(int value) {
+    public MyClass(T value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public T getValue() {
         return value;
     }
 
-    public void setValue(int value) {
+    public void setValue(T value) {
         this.value = value;
     }
 }
